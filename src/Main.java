7ea07a7f968f058213908aeb53935dbf6b6e031d
nataliacatundafr <ncatundafr@gmail.com>
2024-05-02 11:48:02 -0300

    primeiro commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main{
+public class Aluno {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public String nome;
+        private  Float nota;
+
+
+        public Aluno(String nome, float nota){
+          this.nome=nome;
+          this.nota=nota;
         }
-    }
+
+    public Float getNota() {
+        return nota;
+        }
+    public void setNota(float nota){
+
+            }
+
+        }
 }
